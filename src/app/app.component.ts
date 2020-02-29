b3b0255e6855f109b246e9b868a0f40b3ad90121
GaintVlad <sadkoff@tut.by>
2020-02-29 19:51:40 +0300

    section15 Assigment Forms (TD)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
-// import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,45 +7,27 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f', {static: true}) signUpForm: NgForm;
-  defQuestion = 'pet';
-  answer = '';
-  genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secret: '',
-    answer: '',
-    gender: '',
- };
+  @ViewChild('form', {static: true}) submittedForm: NgForm;
+  sEmail: string;
+  sPassword: string;
+  subscription: {value: number, label: string};
   isSubmitted = false;
- suggestUserName() {
-    const suggestedName = 'Superuser';
-    /*this.signUpForm.setValue({
-      userData: {
-        username: suggestedName,
-        email: ''
-      },
-      secret: 'pet',
-      questionAnswer: '',
-      gender: 'male'
-    });*/
-    this.signUpForm.form.patchValue({
-      userData: {
-        username: suggestedName,
-      }
+  subscriptions = [
+    {value: 1, label: 'Basic'},
+    {value: 2, label: 'Advanced'},
+    {value: 3, label: 'Pro'},
+  ];
+  onSubmit(): void {
+    this.isSubmitted = true;
+    console.log(this.submittedForm.value.email);
+    this.sEmail = this.submittedForm.value.email;
+    this.sPassword = this.submittedForm.value.password;
+    this.subscription = this.subscriptions.find(
+      el => el.value === +this.submittedForm.value.subscription
+    );
+    this.submittedForm.reset();
+    this.submittedForm.form.patchValue({
+      subscription: 2
     });
   }
-  // onSubmit(form: NgForm) {
-  //   //   console.log(form);
-  //   // }
-    onSubmit() {
-      this.user.username = this.signUpForm.value.userData.username;
-      this.user.email = this.signUpForm.value.userData.email;
-      this.user.secret = this.signUpForm.value.secret;
-      this.user.answer = this.signUpForm.value.questionAnswer;
-      this.user.gender = this.signUpForm.value.gender;
-      this.isSubmitted = true;
-      this.signUpForm.reset();
-    }
 }
